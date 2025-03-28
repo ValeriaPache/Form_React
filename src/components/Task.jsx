a4794ce2f12156af50e94c1449cbd1c8c65e57c7
Valeria Pacheco <vpachecosanmartin@gmail.com>
2025-03-28 08:23:39 -0500

    I was organizing the styles

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
 import { Form } from 'react-router-dom'
+import './StilesTask.css'
 
 export const Task = () => {
   return (
-    <section >
-        <h1>To-Do List</h1>
-        <form>
-            <input type='Text' placeholder='Hacer Ejercicio'></input>
-            <textarea type='Text' placeholder='Description'></textarea>
-            <button>Add Task</button>
+    <section className='container'>
+        <h1 className='Tittle'>To-Do List</h1>
+        <form className='containerForm'>
+            <input className='inputForm' type='Text' placeholder='Hacer Ejercicio'></input>
+            <textarea className='texttareaForm' type='Text' placeholder='Description'></textarea>
+            <button className='buttomForm'>Add Task</button>
         </form>
-        <div>
-            <div>
-                <button>All</button>
-                <button>Pending</button>
-                <button>Completed</button>
+        <div className='secondContainer'>
+            <div className='containerButtomsFilter'>
+                <button className='buttomsFilter'>All</button>
+                <button className='buttomsFilter' >Pending</button>
+                <button className='buttomsFilter'>Completed</button>
             </div>
-            <button>
+            <button className='containerListTask'>
                 <input type='checkbox'></input>
-                <h3>Barrer</h3>
-                <p>Realizar aseo del lugar</p>
+                <h3 className='titleListTask'>Barrer</h3>
+                <p className='textListTask'>Realizar aseo del lugar</p>
             </button>
-            <button>
+            <button className='containerListTask'>
                 <input type='checkbox'></input>
-                <h3>Estudiar para examen</h3>
-                <p>Preparación bimestral</p>
+                <h3 className='titleListTask'>Estudiar para examen</h3>
+                <p className='textListTask'>Preparación bimestral</p>
             </button>
         </div>
     </section>
